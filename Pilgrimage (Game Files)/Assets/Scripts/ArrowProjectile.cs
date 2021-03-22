2bfa9dabcbaf4d85ddcb9554306e35aba1480872
Abraham Nataniel <abraham.nataniel18@gmail.com>
2021-03-22 09:10:11 +0100

    Added arrow projectiles

---
+++ Pilgrimage (Game Files)/Assets/Scripts/ArrowProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowProjectile : MonoBehaviour
+{
+    private GameObject arrowPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrowPrefab = Resources.Load ("arrow") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newArrow = Instantiate(arrowPrefab) as GameObject;
+            newArrow.transform.position = transform.position;
+            Rigidbody rb = newArrow.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward*30;
+        }
+    }
+}
